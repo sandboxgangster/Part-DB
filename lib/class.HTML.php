09ddf6210065f52aa8403134e07fc081a8123823
Jan Böhmer <jan.h.boehmer@gmx.de>
2016-11-09 20:39:48 +0100

    Added settings dialog and much more.

--- lib/class.HTML.php
+++ lib/class.HTML.php
@@ -23,9 +23,6 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 */
 
-//CHANGE ME: Move to real include dir
-//include("smarty/Smarty.class.php");
-
     /**
      * @file class.HTML.php
      * @brief class HTML
